05e4b03b21e540bbf233ef759292d0686a7f564b
Werner Veldsman <Werner0@users.noreply.github.com>
2023-04-26 11:58:29 +0800

    File checks

--- virus_pipeline.py
+++ virus_pipeline.py
@@ -1,6 +1,6 @@
 # Virus detection pipeline
 # Requirements: metaFlye, viralFlye, PFAM HMM models and a long read mNGS dataset in fasta format
-# See installation instruction at : https://github.com/Werner0/virus_detection_pipeline
+# See installation instructions at: https://github.com/Werner0/virus_detection_pipeline
 # USAGE: virus_pipeline.py -h
 # OUTPUT: Circular and linear virus construct(s). Intermediary files include microbial MAGs.
 
@@ -40,6 +40,7 @@ parser.add_argument('-t', dest='THREADS', type=str, default=str(4),
                     help='Number of CPU threads to use [Default = 4]')
 parser.add_argument('-l', dest='LOGFILE', type=str, default='werner.log', help='Name of log file [Default: werner.log]')
 parser.add_argument('-d', dest='DRYRUN', action='store_true', help='Toggle debug mode')
+parser.add_argument('-m', dest='SKIPMFLYE', action='store_true', help='Skip metaFlye if output is already in metaflye_output directory')
 parser.add_argument('-v', action='version', version='%(prog)s (version 1.0)')
 try:
     args = parser.parse_args()
@@ -60,26 +61,47 @@ def main():
     print("Log file saved to current directory: {}".format(args.LOGFILE))
     logging.info("FASTA file name taken as: {}".format(args.FASTA))
     logging.info("Type of reads taken as: {}".format(args.READTYPE))
-    logging.info("HMM path taken as: {}".format(args.FASTA))
-    logging.info("viralFlye.py path taken as: {}".format(args.FASTA))
+    logging.info("HMM path taken as: {}".format(args.HMM))
+    logging.info("viralFlye.py path taken as: {}".format(args.VIRALFLYE))
     logging.info("Number of CPU threads taken as: {}".format(args.THREADS))
 
     # Check whether metaFlye is installed and run it
-    try:
-        metaflye_version = subprocess.check_output(["flye", "--version"])
-        logging.info("Found metaFlye version " + metaflye_version.decode('utf-8').replace("\n", ""))
-    except:
-        print("The metaFlye executable \"flye\" must be in your path. It does not appear to be.")
-        logging.error("metaflye could not be found in your path")
-        quit()
+    if args.SKIPMFLYE is False:
+        try:
+            metaflye_version = subprocess.check_output(["flye", "--version"])
+            logging.info("Found metaFlye version " + metaflye_version.decode('utf-8').replace("\n", ""))
+        except:
+            print("The metaFlye executable \"flye\" must be in your path. It does not appear to be.")
+            logging.error("metaflye could not be found in your path")
+            quit()
 
     metaflye_command = ["flye", "--"+args.READTYPE, args.FASTA, 
                         "--threads", args.THREADS, "-o", "metaflye_output"]
     metaflye_command_print = " ".join(str(n) for n in metaflye_command)
-    logging.info("metaFlye command: {}".format(metaflye_command_print))
+    if args.SKIPMFLYE is False:
+        logging.info("metaFlye command: {}".format(metaflye_command_print))
     if args.DRYRUN is False:
-        metaflye_output = subprocess.check_output(metaflye_command)
-        logging.info("metaFlye output saved in: {}".format("metaflye_output"))
+        if args.SKIPMFLYE is False:
+            metaflye_output = subprocess.check_output(metaflye_command)
+            Exist = os.path.exists("metaflye_output/assembly.fasta")
+            if Exist:
+                logging.info("metaFlye output saved in directory: metaflye_output")
+            else:
+                logging.error("metaFlye assembly.fasta was not created in metaflye_output")
+                print("Missing output. See log file.")
+                quit()
+        else:
+            Exist = os.path.exists("metaflye_output/assembly.fasta")
+            logging.info("User request to skip metaFlye")
+            if Exist:
+                logging.info("metaFlye assembly.fasta found in directory: metaflye_output")
+                filesize = os.path.getsize("metaflye_output/assembly.fasta")
+                logging.info("assembly.fasta has file size: {}".format(filesize))
+                print("Skipping metaFlye")
+            else:
+                logging.error("metaFlye assembly.fasta is not present in metaflye_output directory. Consider running metaflye first.")
+                print("Missing output. See log file.")
+                quit()
 
     # Run viralFlye
     os.chdir(oscwd)
@@ -89,7 +111,13 @@ def main():
     logging.info("viralFlye command: {}".format(viralflye_command_print))
     if args.DRYRUN is False:
         viralflye_output = subprocess.check_output(viralflye_command)
-        logging.info("viralFlye output saved in: {}".format("viralflye_output"))
+        Exist = os.path.exists("viralflye_output")
+        if Exist:
+            logging.info("viralFlye output saved in directory: viralflye_output")
+        else:
+            logging.error("viralflye_output directory was not created")
+            print("Missing output. See log file.")
+            quit()
     logging.info("Viral detection pipeline has finished")
 
     # Finish
